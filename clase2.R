f9b2041ec5587e07ac9fcc0c77b4e3393fdab7d5
munhofU <116761787+munhofU@users.noreply.github.com>
2023-02-01 12:43:26 -0300

    Add files via upload

--- clase2.R
+++ clase2.R
@@ -51,4 +51,31 @@ lines(x_array,cos(x_array),col="red")
 lines(x_array,sin(x_array),col="blue")
 lines(x_array,cos(x_array**2),col="green")
 
-load("/home/clinux01/Escritorio")
+
+#Ejercio 3
+
+data_auto <- read.table("autos.txt",header = TRUE)
+
+tercera_fila <- data_auto[3,]
+segunda_columna <- data_auto[,2]
+mas_barato <- data_auto[which.min(data_auto$precio),]
+suma_4_filas <- sum(data_auto[1:4,1])
+suma_col<- apply(data_auto, 2 ,sum)
+suma_row<- apply(data_auto, 1 ,sum)
+data_auto_ord <- data_auto[order(data_auto$precio),]
+test <- sort(data_auto$precio,index.return=TRUE)
+test_auto <- data_auto[test$ix,]
+
+plot(data_auto$precio,data_auto$calidad,col="blue", title("precio vs calidad"))
+
+
+#Ejercio 4
+
+data_mt_cars <- mtcars
+mt_cars_4_gears <- data_mt_cars[data_mt_cars$gear==4,]
+mt_cars_4_gears_name <- rownames(mt_cars_4_gears)
+mt_cars_4_gears_y_manual <- data_mt_cars[data_mt_cars$gear==4 & data_mt_cars$am==1,]
+mt_cars_4_gears_y_manual_len <- length(rownames(mt_cars_4_gears_y_manual)) 
+mt_cars_4_gears_o_manual <- data_mt_cars[data_mt_cars$gear==4 | data_mt_cars$am==1,]
+mt_cars_4_gears_o_manual_len <- length(rownames(mt_cars_4_gears_o_manual))
+data_mt_cars$am <- factor(data_mt_cars$am)
